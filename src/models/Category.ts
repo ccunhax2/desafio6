9c832bcbe2a523940031dc4a326df1c973612016
ccunhax2 <56969622+ccunhax2@users.noreply.github.com>
2021-03-29 02:57:16 -0300

    Update Category.ts
    Teste de Merge

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,5 +1,5 @@
 import {
-    Entity,
+    EntityAlterado,
     Column,
     PrimaryGeneratedColumn,
     CreateDateColumn,
@@ -14,11 +14,10 @@ class Category {
     @Column()
     title: string;
 
-    @CreateDateColumn()
-    created_at: Date;
-
     @UpdateDateColumn()
     updated_at: Date;
+    
+    DummyIncluido: true;
 }
 
 export default Category;
